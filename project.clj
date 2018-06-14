a2b5e9b1fb50e2b8f08a69989e06de5e41781f7b
sumanmaity112 <suman.maity112@gmail.com>
2018-06-14 20:42:22 +0530

    Suman | #3 | Narrow down to ring-jetty-adapter as dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   {:name "Eclipse Public License"
    :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.7.0-RC1"]
+                 [ring/ring-jetty-adapter "1.7.0-RC1"]
                  [compojure "1.6.1"]
                  [clj-time "0.14.2"]
                  [org.clojure/java.jdbc "0.7.6"]
